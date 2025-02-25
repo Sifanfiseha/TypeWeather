2c7545feff3febeb2f401af42890c01a99193640
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-02-24 23:02:12 -0500

    netlify test 10

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const searchInput = document.querySelector("#searchInput");
+const link = document.querySelector(".redirect-link");
+//  document.querySelector("form").addEventListener("submit", (e) => {
+//         e.preventDefault();
+//         const city = searchInput.value.trim();
+//         sessionStorage.setItem("city", city);
+//         // if (city) {
+//         //   window.location.assign(
+//         //     `/content.html?city=${encodeURIComponent(city)}`
+//         //   );
+//         // } else {
+//         //   alert("please enter a city name!");
+//         // }
+//         // window.location.href = "content.html";
+//         link.href = `./content.html?city=${encodeURIComponent(city)}`;
+//         link.click();
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = searchInput.ariaValueMax;
+  link.href = `./content.html?city=${encodeURIComponent(city)}`;
+  console.log(city);
+});
